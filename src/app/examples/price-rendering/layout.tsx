1d6cca8521a8cb7a975438f2315567daa84f3189
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-04-15 17:50:41 -0400

    fix: title

--- src/app/examples/price-rendering/layout.tsx
+++ src/app/examples/price-rendering/layout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react'
 export const metadata: Metadata = {
   title: {
     template: '%s - Learn_Frontend',
-    default: 'Learn_Frontend - Render prices',
+    default: 'Render prices',
   },
   description: `This platform is not like other programming courses. It won't teach you how to code, but it will teach you how to code better. It gathers the best practices in front-end development to help you become a better developer. Hope it will be useful to you!`,
 }
